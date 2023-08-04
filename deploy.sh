ea050ff01a204138aeaa4a62c9bf7a0325e5676c
Nito24 <84534938+Nito24@users.noreply.github.com>
2023-08-04 12:00:36 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -45,7 +45,7 @@
 # Custom icons
 
 LIST_OF_DEVEL_APPS="base-devel git make wget curl fakeroot p7zip neovim man" #git is not really needed since thats how you have suposedly downloaded the config. Same goes with nvim, it should already be installed
-LIST_OF_APPS="neofetch xf86-video-nouveau xorg-server xorg-xinit qtile picom archlinux-wallpaper unclutter dunst xbindkeys alacritty firefox alsa-utils pulseaudio pulseaudio-alsa volumeicon zsh lsd powerline-fonts"
+LIST_OF_APPS="neofetch xf86-video-nouveau xorg-server xorg-xinit qtile picom archlinux-wallpaper unclutter dunst xbindkeys brightnessctl alacritty firefox alsa-utils pulseaudio pulseaudio-alsa volumeicon zsh lsd powerline-fonts"
 LIST_OF_APPS_AUR="qtile-extras" # spotify spicetify-cli
 
 
